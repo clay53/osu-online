722f99d73b4e9fb992640743881667258dd3c00a
clay53 <clayton@claytondoesthings.xyz>
2020-01-03 15:53:46 +0000

    Added comment

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config = {
     backgroundDim: 50,
     enlarge: false,
     verticalTextOffset: 2.5,
-    ACScale: 2,
+    ACScale: 2, // Approach Circle Scale
     disableVideo: false,
     progressionBarMethod: "line"
 }
